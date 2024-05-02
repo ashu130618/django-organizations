2b13a14a588b05f2473ad59c33439dc1faee2441
ashu130618 <150803176+ashu130618@users.noreply.github.com>
2024-05-02 11:37:43 +0530

    Update signals.py

--- organizations/signals.py
+++ organizations/signals.py
@@ -25,9 +25,14 @@
 
 import django.dispatch
 
-user_kwargs = {"providing_args": ["user"]}
-user_added = django.dispatch.Signal(**user_kwargs)
-user_removed = django.dispatch.Signal(**user_kwargs)
+# user_kwargs = {"providing_args": ["user"]}
+# user_added = django.dispatch.Signal(**user_kwargs)
+# user_removed = django.dispatch.Signal(**user_kwargs)
 
-owner_kwargs = {"providing_args": ["old", "new"]}
-owner_changed = django.dispatch.Signal(**owner_kwargs)
+# owner_kwargs = {"providing_args": ["old", "new"]}
+# owner_changed = django.dispatch.Signal(**owner_kwargs)
+
+user_added = django.dispatch.Signal()
+user_removed = django.dispatch.Signal()
+
+owner_changed = django.dispatch.Signal()
